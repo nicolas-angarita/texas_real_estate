c4f6299e8fa6391da5a6ef983594e016437e2a3b
Nicolas Angarita <nicolas.angarita07@gmail.com>
2023-05-24 14:26:06 -0500

    explore

--- explore.py
+++ explore.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np 
 import seaborn as sns
-import matplotlob.pyplot as plt
+import matplotlib.pyplot as plt
 from datetime import datetime, timedelta 
 from scipy.stats import pearsonr
 
